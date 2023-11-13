f74b43d2968db30ee64f96920612e6b5eb85751a
Jacob Korb <jacob.korb@gmail.com>
2023-11-12 23:12:06 -0500

    change nav menu to functional

--- WebApp/src/components/NavMenu.js
+++ WebApp/src/components/NavMenu.js
@@ -1,33 +1,14 @@
-import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+const NavMenu = () => {
+  return (
+    <header>
+      <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+        <NavbarBrand tag={Link} to="/">Qwizardry</NavbarBrand>
+      </Navbar>
+    </header>
+  );
+};
 
-  constructor (props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render() {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">Qwizardry</NavbarBrand>
-
-        </Navbar>
-      </header>
-    );
-  }
-}
+export default NavMenu;
